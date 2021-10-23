f34e46089750d223b173cd37ba549fca14c46058
Tony Edgal <78075405+tonyedgal@users.noreply.github.com>
2021-10-23 17:01:18 +0100

    Fixed responsive navbar

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 $(document).ready(function () {
   $(".toggle").click(function () {
-    $(".toggle").toggleClass("active");
-    $("nav").toggleClass("active");
-  });
-});
+    $(".toggle").toggleClass("active")
+    $("nav").toggleClass("active")
+  })
+})
 
 $("#landingBtn").on("click", function () {
-  const imageGallery = $("#imageGallery").position().top;
+  const imageGallery = $("#imageGallery").position().top
 
   $("html, body").animate(
     {
       scrollTop: imageGallery,
     },
     3000
-  );
-});
+  )
+})
